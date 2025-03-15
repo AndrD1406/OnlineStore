1631a24e6c3785233aad3fa29e1f1af3110ae377
Danylo Kozlovskyi <danilokozlovskij@gmail.com>
2025-03-15 20:38:29 +0200

    started writing cart service

---
+++ OnlineStore.BusinessLogic/Services/Interfaces/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.BusinessLogic.Services.Interfaces
+{
+    public interface ICartService
+    {
+        Task<Cart> AddProduct();
+    }
+}
