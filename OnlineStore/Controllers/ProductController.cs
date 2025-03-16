5f37622141154d405954acef44b49144e5f89f03
Danylo Kozlovskyi <danilokozlovskij@gmail.com>
2025-03-16 22:22:01 +0200

    added filtering by price and fixed some binding issues with search component

--- OnlineStore/Controllers/ProductController.cs
+++ OnlineStore/Controllers/ProductController.cs
@@ -30,7 +30,10 @@ public class ProductController : Controller
         var user = User;
         var stores = await storeService.GetAll();
         ViewBag.Stores = stores;
-        var products = await productService.Filter(x => product != null ? x.Name.ToLower().Contains(product.ToLower()) : true && storeId != null ? x.StoreId == storeId : true && price != null ? x.Price <= price : true);
+        ViewBag.storeId = storeId;
+        ViewBag.product = product;
+        ViewBag.price = price;
+        var products = await productService.Filter(x => (product != null ? x.Name.ToLower().Contains(product.ToLower()) : true) && (storeId != null ? x.StoreId == storeId : true) && (price != null ? x.Price <= price : true));
         return View(products);
     }
     [HttpGet]
