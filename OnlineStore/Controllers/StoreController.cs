dbbf295dcab9211f099fc81601b5962d8dc041ea
Inn21 <Innicasandretorz@gmail.com>
2025-03-17 17:51:59 +0200

    Added logic for view store inventory

--- OnlineStore/Controllers/StoreController.cs
+++ OnlineStore/Controllers/StoreController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineStore.BusinessLogic.Services.Interfaces;
+using OnlineStore.Models;
 
 namespace OnlineStore.Controllers;
 [Route("[controller]/[action]")]
@@ -15,9 +16,35 @@ public class StoreController : Controller
         this.productService = productService;
         this.storeService = storeService;
     }
-
-    public IActionResult Index()
+    [HttpGet]
+    //[Authorize]
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var stores = await storeService.GetAll();
+        return View(stores);
+    }
+
+    public async Task<IActionResult> Details(Guid storeId, string? product, double? price)
+    {
+        var store = await storeService.Get(storeId);
+        if (store == null)
+        {
+            return NotFound();
+        }
+
+        var products = await productService.Filter(x =>
+            x.StoreId == storeId &&
+            (string.IsNullOrEmpty(product) || x.Name.ToLower().Contains(product.ToLower())) &&
+            (!price.HasValue || x.Price <= price));
+
+        var viewModel = new StoreDetailsViewModel
+        {
+            Store = store,
+            Products = products,
+            ProductFilter = product,
+            PriceFilter = price
+        };
+
+        return View(viewModel);
     }
 }
