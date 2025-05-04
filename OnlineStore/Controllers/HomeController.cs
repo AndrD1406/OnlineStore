af9ca499a40f2b60dd19e5805bd467de163a5786
DanyloKozlovskyi <danilokozlovskij@gmail.com>
2025-05-04 11:07:07 +0300

    added validation on login and register forms

--- OnlineStore/Controllers/HomeController.cs
+++ OnlineStore/Controllers/HomeController.cs
@@ -48,7 +48,7 @@ public class HomeController : Controller
     {
         if (await userManager.Users.AnyAsync(u => u.Email == registerDto.Email))
             ModelState.AddModelError("Email", "Email address is already taken.");
-        
+
         // Validation 
         if (!ModelState.IsValid)
         {
@@ -111,7 +111,8 @@ public class HomeController : Controller
         if (!ModelState.IsValid)
         {
             string errorMessages = string.Join(" | ", ModelState.Values.SelectMany(x => x.Errors).Select(e => e.ErrorMessage));
-            return Problem(errorMessages);
+            ViewBag.Errors = errorMessages;
+            return View(loginDto);
         }
 
         var result = await signInManager.PasswordSignInAsync(loginDto.Email, loginDto.Password, isPersistent: false, lockoutOnFailure: false);
@@ -126,204 +127,211 @@ public class HomeController : Controller
             await signInManager.SignInAsync(user, isPersistent: false);
             await userManager.UpdateAsync(user);
 
-                var roles = await userManager.GetRolesAsync(user);
-                var claims = new List<Claim>
+            var roles = await userManager.GetRolesAsync(user);
+            var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                     new Claim(ClaimTypes.Name, user.Email),
                     new Claim(ClaimTypes.Role,  roles.FirstOrDefault())
                 };
-                ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), new AuthenticationProperties() { IsPersistent = false });
-                return RedirectToAction("Index", "Product");
-            }
-            return Problem("Invalid email or password");
-        }
-        [Authorize]
-        public async Task<IActionResult> LogOut()
-        {
-            await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), new AuthenticationProperties() { IsPersistent = false });
             return RedirectToAction("Index", "Product");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        ModelState.AddModelError("Email", "Invalid email or password");
+        ModelState.AddModelError("Password", "Invalid email or password");
+        var errors = string.Join(" | ", ModelState.Values.SelectMany(x => x.Errors).Select(e => e.ErrorMessage));
+        ViewBag.Errors = errors;
+
+        return View(loginDto);
+    }
+    [Authorize]
+    public async Task<IActionResult> LogOut()
+    {
+        await HttpContext.SignOutAsync(IdentityConstants.ApplicationScheme);
+        return RedirectToAction("Index", "Product");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> ModifyPersonalAccount()
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await userManager.FindByIdAsync(userId);
+        var registerDto = new RegisterDto() { Name = user.Name, Email = user.Email, PhoneNumber = user.PhoneNumber };
+
+        return View(registerDto);
+    }
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> ModifyPersonalAccount(RegisterDto updatedUser)
+    {
+        // Validation 
+        if (!ModelState.IsValid)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            string errorMessages = string.Join(" | ", ModelState.Values.SelectMany(x => x.Errors).Select(e => e.ErrorMessage));
+            ViewBag.Errors = errorMessages;
+            return View(updatedUser);
         }
-        [HttpGet]
-        [Authorize]
-        public async Task<IActionResult> ModifyPersonalAccount()
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user = await userManager.FindByIdAsync(userId);
-            var registerDto = new RegisterDto() { Name=user.Name, Email=user.Email, PhoneNumber=user.PhoneNumber };
 
-            return View(registerDto);
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser user = new ApplicationUser();
+
+        user = await userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            return NotFound("User not found");
         }
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> ModifyPersonalAccount(RegisterDto updatedUser)
+        try
         {
-            // Validation 
-            if (!ModelState.IsValid)
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+            user.PhoneNumber = updatedUser.PhoneNumber;
+            user.UserName = updatedUser.Email;
+
+            var resultUpdateUser = await userManager.UpdateAsync(user);
+            if (!resultUpdateUser.Succeeded)
             {
-                string errorMessages = string.Join(" | ", ModelState.Values.SelectMany(x => x.Errors).Select(e => e.ErrorMessage));
-                return Problem(errorMessages);
+                string errorMessage = string.Join(" | ", resultUpdateUser.Errors.Select(e => e.Description));
+                return Problem(errorMessage);
             }
 
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            ApplicationUser user = new ApplicationUser();
-
-            user = await userManager.FindByIdAsync(userId);
-            if (user == null)
+            var resultRemovePasword = await userManager.RemovePasswordAsync(user);
+            if (!resultRemovePasword.Succeeded)
             {
-                return NotFound("User not found");
-            }
-            try
-            {
-                user.Name = updatedUser.Name;
-                user.Email = updatedUser.Email;
-                user.PhoneNumber = updatedUser.PhoneNumber;
-                user.UserName = updatedUser.Email;
-
-                var resultUpdateUser  = await userManager.UpdateAsync(user);
-                if(!resultUpdateUser.Succeeded)
-                {
-                    string errorMessage = string.Join(" | ", resultUpdateUser.Errors.Select(e => e.Description));
-                    return Problem(errorMessage);
-                }
-
-                var resultRemovePasword = await userManager.RemovePasswordAsync(user);
-                if (!resultRemovePasword.Succeeded)
-                {
-                    string errorMessage = string.Join(" | ", resultRemovePasword.Errors.Select(e => e.Description));
-                    return Problem(errorMessage);
-                }
-
-                var resultAddPassword = await userManager.AddPasswordAsync(user, updatedUser.Password);
-                if (!resultAddPassword.Succeeded)
-                {
-                    string errorMessage = string.Join(" | ", resultAddPassword.Errors.Select(e => e.Description));
-                    return Problem(errorMessage);
-                }
-            }
-            catch (Exception ex)
-            {
-                return Problem(ex.Message);
+                string errorMessage = string.Join(" | ", resultRemovePasword.Errors.Select(e => e.Description));
+                return Problem(errorMessage);
             }
 
-            await signInManager.RefreshSignInAsync(user);
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            var claims = new List<Claim>
+            var resultAddPassword = await userManager.AddPasswordAsync(user, updatedUser.Password);
+            if (!resultAddPassword.Succeeded)
+            {
+                string errorMessage = string.Join(" | ", resultAddPassword.Errors.Select(e => e.Description));
+                return Problem(errorMessage);
+            }
+        }
+        catch (Exception ex)
+        {
+            return Problem(ex.Message);
+        }
+
+        await signInManager.RefreshSignInAsync(user);
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                     new Claim(ClaimTypes.Name, user.Email),
                     new Claim(ClaimTypes.Role, updatedUser.IsAdmin ? "Admin" : "User")
                 };
-            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), new AuthenticationProperties() { IsPersistent = false });
+        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), new AuthenticationProperties() { IsPersistent = false });
 
-            return RedirectToAction("Index", "Product");
+        return RedirectToAction("Index", "Product");
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Users([FromQuery] string? name, [FromQuery] string? email)
+    {
+        ViewBag.name = name;
+        ViewBag.email = email;
+        var users = await userManager.Users.Where(x => (name != null ? x.Name.ToLower().Contains(name.ToLower()) : true) && (email != null ? x.Email.ToLower().Contains(email.ToLower()) : true)).ToListAsync();
+
+
+        List<object> userList = new List<object>();
+        foreach (var user in users)
+        {
+            var roles = await userManager.GetRolesAsync(user);
+            userList.Add(new
+            {
+                user.Id,
+                Name = user.Name,
+                user.Email,
+                user.PhoneNumber,
+                Role = roles.FirstOrDefault()
+            });
+        }
+        return View(userList);
+    }
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        if (string.IsNullOrEmpty(id))
+        {
+            return NotFound();
         }
 
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> Users([FromQuery] string? name, [FromQuery] string? email)
+        var user = await userManager.FindByIdAsync(id);
+        if (user == null)
         {
-            ViewBag.name = name;
-            ViewBag.email = email;
-            var users = await userManager.Users.Where(x => (name != null ? x.Name.ToLower().Contains(name.ToLower()) : true) && (email != null ? x.Email.ToLower().Contains(email.ToLower()) : true)).ToListAsync();
-
-
-            List<object> userList = new List<object>();
-            foreach (var user in users)
-            {
-                var roles = await userManager.GetRolesAsync(user);
-                userList.Add(new
-                {
-                    user.Id,
-                    Name = user.Name,
-                    user.Email,
-                    user.PhoneNumber,
-                    Role = roles.FirstOrDefault() 
-                });
-            }
-            return View(userList);
+            return NotFound();
         }
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> DeleteUser(string id)
+
+        var result = await userManager.DeleteAsync(user);
+
+        if (result.Succeeded)
         {
-            if (string.IsNullOrEmpty(id))
-            {
-                return NotFound();
-            }
+            return RedirectToAction("Users");
+        }
 
-            var user = await userManager.FindByIdAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
+        string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description));
+        return Problem(errorMessage);
+    }
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public IActionResult CreateUser()
+    {
+        return View();
+    }
 
-            var result = await userManager.DeleteAsync(user);
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateUser(RegisterDto registerDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            string errorMessages = string.Join(" | ", ModelState.Values.SelectMany(x => x.Errors).Select(e => e.ErrorMessage));
+            return Problem(errorMessages);
+        }
+
+        ApplicationUser user = new ApplicationUser() { Id = Guid.NewGuid(), Name = registerDto.Name, Email = registerDto.Email, PhoneNumber = registerDto.PhoneNumber, UserName = registerDto.Email };
+
+        IdentityResult result = null;
+        try
+        {
+            result = await userManager.CreateAsync(user, registerDto.Password);
 
             if (result.Succeeded)
             {
-                return RedirectToAction("Users"); 
-            }
-
-            string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description));
-            return Problem(errorMessage);
-        }
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public IActionResult CreateUser()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [Authorize(Roles = "Admin")]
-        public async Task<IActionResult> CreateUser(RegisterDto registerDto)
-        {
-            if (!ModelState.IsValid)
-            {
-                string errorMessages = string.Join(" | ", ModelState.Values.SelectMany(x => x.Errors).Select(e => e.ErrorMessage));
-                return Problem(errorMessages);
-            }
-
-            ApplicationUser user = new ApplicationUser() { Id = Guid.NewGuid(), Name = registerDto.Name, Email = registerDto.Email, PhoneNumber = registerDto.PhoneNumber, UserName = registerDto.Email };
-
-            IdentityResult result = null;
-            try
-            {
-                result = await userManager.CreateAsync(user, registerDto.Password);
-
-                if (result.Succeeded)
+                if (registerDto.IsAdmin)
                 {
-                    if (registerDto.IsAdmin)
-                    {
-                        await userManager.AddToRoleAsync(user, "Admin");
-                    }
-                    else
-                    {
-                        await userManager.AddToRoleAsync(user, "User");
-                    }
+                    await userManager.AddToRoleAsync(user, "Admin");
+                }
+                else
+                {
+                    await userManager.AddToRoleAsync(user, "User");
                 }
             }
-            catch (Exception exc)
-            {
-                Console.WriteLine(exc.Message);
-            }
-
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Users");
-            }
-
-            string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description));
-            return Problem(errorMessage);
         }
-    
+        catch (Exception exc)
+        {
+            Console.WriteLine(exc.Message);
+        }
+
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Users");
+        }
+
+        string errorMessage = string.Join(" | ", result.Errors.Select(e => e.Description));
+        return Problem(errorMessage);
+    }
+
 }
