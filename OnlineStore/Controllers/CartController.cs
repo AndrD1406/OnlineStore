84a79e759c0d2ca2838a78b9ed5afa40a09b811e
DanyloKozlovskyi <danilokozlovskij@gmail.com>
2025-05-12 21:31:43 +0300

    added pagination on cart/index page and redesigned it

--- OnlineStore/Controllers/CartController.cs
+++ OnlineStore/Controllers/CartController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using OnlineStore.BusinessLogic.Services;
 using OnlineStore.BusinessLogic.Services.Interfaces;
 using OnlineStore.DataAccess.Models;
+using System.Drawing.Printing;
+using System.Linq.Expressions;
 using System.Security.Claims;
 
 namespace OnlineStore.Controllers;
@@ -10,7 +12,7 @@ namespace OnlineStore.Controllers;
 [Route("[controller]/[action]")]
 public class CartController : Controller
 {
-
+    private const int PAGES_RANGE_SIZE = 8;
     private readonly ICartService _cartService;
 
     public CartController(ICartService cartService)
@@ -19,7 +21,7 @@ public class CartController : Controller
     }
     [HttpGet]
     [Authorize]
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(int page = 1, int pageSize = 8)
     {
         var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
         if (!Guid.TryParse(userIdString, out Guid userId))
@@ -27,12 +29,32 @@ public class CartController : Controller
             return Unauthorized();
         }
 
+        Expression<Func<Cart, bool>> filterExpression = c => c.UserId == userId;
+
         var carts = await _cartService.GetByUserId(userId);
         var cart = carts.FirstOrDefault();
         if (cart == null)
         {
             cart = await _cartService.Create(new Cart { Id = Guid.NewGuid(), UserId = userId });
         }
+
+        int totalCartProducts = cart.ProductToCarts.DistinctBy(x => x.ProductId).Count();
+        int totalPages = (int)Math.Ceiling((double)totalCartProducts / pageSize);
+
+        int startPage = Math.Max(1, page - PAGES_RANGE_SIZE / 2);
+        int endPage = Math.Min(totalPages, startPage + PAGES_RANGE_SIZE - 1);
+
+        if (endPage - startPage + 1 < PAGES_RANGE_SIZE)
+        {
+            startPage = Math.Max(1, endPage - PAGES_RANGE_SIZE + 1);
+        }
+
+        ViewBag.CurrentPage = page;
+        ViewBag.PageSize = pageSize;
+        ViewBag.TotalPages = totalPages;
+        ViewBag.StartPage = startPage;
+        ViewBag.EndPage = endPage;
+        ViewBag.ActionName = nameof(Index);
 
         return View(cart);
     }
